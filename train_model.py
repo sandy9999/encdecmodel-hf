ad7219022264e6d46b069dc710e2549b82c58665
Sandhya Saravanan <luckysandy99@gmail.com>
2021-02-18 11:32:54 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -74,7 +74,8 @@ decoder_config = BertConfig(vocab_size = vocabsize,
                     num_hidden_layers = decparams["num_hidden_layers"],
                     hidden_size = decparams["hidden_size"],
                     type_vocab_size = 1,
-                    is_decoder=True)    # Very Important
+                    is_decoder=True,
+                    add_cross_attention=True)    # Very Important
 
 decoder = BertForMaskedLM(config=decoder_config)
 
